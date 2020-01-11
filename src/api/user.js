92951e1cea0e2f5ebfdd14b8bc0a4f7c26eeb330
tom_glb <goodMorning_glb@yukaifan.com>
2020-01-11 17:34:22 +0800

    YK-iSystem-Vue 前台管理系统

--- src/api/user.js
+++ src/api/user.js
@@ -2,17 +2,23 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/vue-element-admin/user/login',
+    url: '/auth/api/login/token',
     method: 'post',
     data
   })
 }
 
-export function getInfo(token) {
+export function getUserInfo(token) {
   return request({
-    url: '/vue-element-admin/user/info',
-    method: 'get',
-    params: { token }
+    url: '/auth/api/systemUserInfo/getUserProfile',
+    method: 'get'
+  })
+}
+
+export function getSystemUserList() {
+  return request({
+    url: '/admin/api/systemUser/getSystemUsers',
+    method: 'get'
   })
 }
 
